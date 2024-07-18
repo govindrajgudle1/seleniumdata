d859643f5b481cf275d909e7b62dce3af0e16943
Govindraj Chandrashekhar Gudle <118347655+govindrajgudle1@users.noreply.github.com>
2024-07-18 14:48:53 +0530

    Add files via upload

--- pythonflask.py
+++ pythonflask.py
@@ -3,7 +3,7 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-
+# Configure Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.yourmailserver.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'your-email@example.com'
@@ -17,12 +17,35 @@ mail = Mail(app)
 def send_email():
     msg = Message('Python (Selenium) Assignment - Govindraj Gudle',
                   sender='your-email@example.com',
-                  recipients=['tech@themedius.ai', 'hr@themedius.ai'])
-    msg.body = 'Please find attached the assignment submission.'
+                  recipients=['tech@themedius.ai'],
+                  cc=['hr@themedius.ai'])
+    
+    msg.body = """\
+Dear Team,
 
+Please find attached the following items for the assignment submission:
+1. Screenshot of the form filled via code.
+2. Source code - https://github.com/govindrajgudle1/seleniumdata.git
+3. Brief documentation of my approach.
+4. My resume.
+5. Links to past projects/work samples.
+6. Confirming my availability to work full-time (10 am to 7 pm) for the next 3-6 months.
+
+Thank you.
+
+Best regards,
+Govindraj Gudle
+
+GitHub Repository: https://github.com/govindrajgudle1/seleniumdata.git
+"""
+
+    # Attach files
     attachments = [
         {"path": "form_filled.png", "name": "form_filled.png", "type": "image/png"},
-        {"path": "resume.pdf", "name": "govindrajscv.pdf", "type": "application/pdf"}    ]
+        {"path": "govindrajscv.pdf", "name": "Govindraj_Gudle_Resume.pdf", "type": "application/pdf"},
+        {"path": "documentation.pdf", "name": "documentaion", "type": "text/markdown"}
+        # Add more attachments if needed
+    ]
 
     for attachment in attachments:
         with app.open_resource(attachment["path"]) as fp:
